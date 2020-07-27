e362f358fde967cff79e819b09102dac67056130
gonzo8880 <66904713+gonzo8880@users.noreply.github.com>
2020-07-26 20:50:54 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,4 +2,57 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
+} import java.util.Scanner;
+
+class MyClass {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+    String username;
+
+    System.out.println("Enter username");
+    userName = myObj.nextline();
+
+    System.out.println("Username is:gonzo8880"+ userName);
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
+
+public class MyClass.java {
+  public static void main(String[] args) {
+    String name = "Gonzalo";
+    System.out.println(name);
+  }
+}
+
+public class MyClass.java {
+  public static void main(String[] args) {
+    int myNum = 6;
+    System.out.println(myNum);
+  }
+}
+
+public class MyClass.java {
+  public static void main(String[] args) {
+    float myFloatNum = 10.99f
+    System.out.println(myFloatNum);
+  }  
+}
+
+public class MyClass.java {
+  public static void main(String[] args) {
+    char myLetter = 'G';
+    System.out.println(myLetter);
+  }
+} 
+
+public class MyClass.java{
+  public static void main(String[] args) {
+    String myText = "Enjoy yourself";
+    System.out.println(MyText);
+  }
 }
